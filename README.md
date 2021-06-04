076a865d61590b54c99d991ab149a4e9ed8b3574
Yuto Watanabe <yuto.w51942@gmail.com>
2021-06-04 13:23:47 +0900

    done

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# my-pc-utils
+# My PC Utils
+
+## アップデート
+
+- HomeBrew
+- Prezto
+- Goenv
+- SDKMAN!
+
+```bash
+curl -fsSL https://raw.githubusercontent.com/yuto51942/my-pc-utils/main/update/updater.sh | zsh
+```
--- update/updater.sh
+++ update/updater.sh
@@ -16,8 +16,8 @@ function initialize () {
     export PATH="$PATH:$GOPATH/bin"
 
     #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-    export SDKMAN_DIR="~/.sdkman"
-    [[ -s "~/.sdkman/bin/sdkman-init.sh" ]] && source "~/.sdkman/bin/sdkman-init.sh"
+    export SDKMAN_DIR="$HOME/.sdkman"
+    [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 }
 
 function update_brew () {
